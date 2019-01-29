b1b1f7e7ee2b23bb417c98c056fc4f28aa028df5
Shankar <shankar.acharya524@gmail.com>
2019-01-29 20:37:06 +0545

    refactored code for arr_sort and array_highest2_sum file

--- lib/array_highest2_sum.rb
+++ lib/array_highest2_sum.rb
@@ -1,11 +1,7 @@
 class ArrayHighest2Sum
 
-def calculate(arr)
-  arr.sort!
-  arr[arr.length-1]+arr[arr.length-2]
-end#calculate method ends here
-
-def get_highest_value(arr)
-  arr.max
+  def calculate(arr)
+    arr.sort!
+    arr[arr.length-1]+arr[arr.length-2]
+  end
 end
-end#class ends here
--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -1,5 +1,5 @@
 class ArraySort 
   def sort(arr)
     arr.sort
-  end#sort function ends here
-end #class definition ends here
+  end
+end 
